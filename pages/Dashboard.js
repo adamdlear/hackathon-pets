c7fd7f0269664822d88c7ccdecac75a120dbfbc9
Adam Lear <adamlear12@gmail.com>
2024-02-18 01:54:53 -0600

    add progress chart to dash

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,17 +1,49 @@
 import { Text, View, StyleSheet, SafeAreaView, Pressable } from 'react-native';
+import { ProgressChart } from 'react-native-chart-kit';
 import InputButton from "../components/InputButton"
 
 const Dashboard = ({navigation}) => {
+    const foodData = {
+        labels: ["Calories"],
+        data: [.50]
+    }
+    const exerciseData = {
+        data: [.25]
+    }
     return (
         <SafeAreaView style={{backgroundColor: "white", height: "100%"}}>   
             <Text style={styles.petName}>Fido</Text>
             <View style={styles.container}>
                 <View style={styles.box}>
                     <Text style={styles.boxHeader}>Food</Text>
+                    <ProgressChart
+                        data={foodData}
+                        width={125}
+                        height={125}
+                        chartConfig={{
+                            backgroundGradientFromOpacity: 0,
+                            backgroundGradientToOpacity: 0,
+                            color: (opacity = 1) => `rgba(116, 148, 252, ${opacity})`,
+                            strokeWidth: 10, // optional, default 3
+                        }}
+                        hideLegend={true}
+                    />
                     <InputButton title="Input Food"></InputButton>
                 </View>
                 <View style={styles.box}>
                     <Text style={styles.boxHeader}>Exercise</Text>
+                    <ProgressChart
+                        data={exerciseData}
+                        width={125}
+                        height={125}
+                        chartConfig={{
+                            backgroundGradientFromOpacity: 0,
+                            backgroundGradientToOpacity: 0,
+                            color: (opacity = 1) => `rgba(116, 148, 252, ${opacity})`,
+                            strokeWidth: 10, // optional, default 3
+                        }}
+                        hideLegend={true}
+                    />
                     <InputButton title="Input Exercise"></InputButton>
                 </View>
             </View>
@@ -37,7 +69,7 @@ const styles = StyleSheet.create({
     box: {
         height: 300,
         width: 150,
-        backgroundColor: "lightergrey",
+        backgroundColor: "lightgray",
         borderRadius: 5,
         alignItems: "center",
     },
